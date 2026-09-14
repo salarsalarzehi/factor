30af5c07f58282660b8a6bf31528c2e0be7b9683
salarsalarzehi <164910100+salarsalarzehi@users.noreply.github.com>
2026-09-14 19:02:17 +0330

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function addRow() {
     let newRow = document.createElement("tr");
     newRow.innerHTML = `
         <td>${toPersianNum(rowCount)}</td>
-        <td><input type="textarea" placeholder="شرح کالا"></td>
+        <td><textarea class="item-desc" placeholder="شرح کالا"></textarea></td>
         <td><input type="number" class="price" value="0" oninput="calculate()"></td>
         <td><input type="number" class="qty" value="1" oninput="calculate()"></td>
         <td><input type="number" class="discount" value="0" oninput="calculate()"></td>
